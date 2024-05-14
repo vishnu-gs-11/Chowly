c7fbce40df635c664de423562987560b9b6ce42a
Vishnu Ganesan <115076749+vishnu-gs-11@users.noreply.github.com>
2024-05-14 18:57:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Chowly from "./components/Chowly";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Chowly />
+      </div>
+    </div>
+  );
+}
+
+export default App;
